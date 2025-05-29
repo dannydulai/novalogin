5ff3b901bafd1472d1755d1f53638ee9a97feb1d
Your Name <nschairer@gmail.com>
2025-05-29 17:18:54 -0400

    comment out old apple code

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,5 @@
 <template>
   <div id="app" class="h-screen w-full">
-    <nav class="p-4 bg-slate-100 flex gap-4">
-      <router-link to="/login">Login</router-link>
-      <router-link to="/account">Account</router-link>
-      <router-link to="/admin">Admin</router-link>
-    </nav>
     <main class="p-4">
       <router-view />
     </main>
