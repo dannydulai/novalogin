0784abaf76cf9701088d79c17781fa560b4733f8
Your Name <nschairer@gmail.com>
2025-05-29 16:58:30 -0400

    frontend shell

---
+++ frontend/src/App.vue
@@ -0,0 +1,6 @@
+<template>
+  <div id="app" class="h-screen w-full flex justify-center items-center text-6xl text-slate-900">
+      <span class="mdi mdi-home"></span>
+      Hello World
+  </div>
+</template>
---
+++ frontend/vite.config.js
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { fileURLToPath, URL } from 'node:url';
+import tailwindcss from '@tailwindcss/vite'
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+    vue()
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  //server: {
+  //  port: 3001,
+  //  proxy: {
+  //    '/api': {
+  //      target: 'http://localhost:3000',
+  //      changeOrigin: true
+  //    }
+  //  }
+  //}
+});
+
